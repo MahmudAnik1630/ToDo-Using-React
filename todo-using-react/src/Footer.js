b4eae790ef1b91c232dff003483d9328d566a125
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-07-11 00:12:35 +0600

    Update Footer.js

--- todo-using-react/src/Footer.js
+++ todo-using-react/src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
+    <footer className="bg-amber-700 text-white p-4 mt-8">
       <div className="max-w-md mx-auto text-center">
         <p>&copy; 2025 My Todo App. Simple and Easy!</p>
       </div>
